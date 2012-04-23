968e6deb7b5e7cf6ca805180996cd9dec352bb20
Raphael Doehring <raphael.doehring@gmail.com>
2012-04-23 08:12:47 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RssTools::Application.config.secret_token = '7f27da32eea3203ca0be8c7916653a6bfb6f821e8687b5eb9591f43ab29bace2545fbf530ffc98b9c196781e7bf117a957794222bbe8387b98e3aba33c1a65a1'
