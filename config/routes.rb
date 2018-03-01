981dfce103c0724bec8796e347200617250c6e9f
Raphael Doehring <raphael.doehring@gmail.com>
2018-03-01 20:11:19 +0100

    welcome to rails 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #
+  #
+  resources :enhancers
+
+  root :to => 'hs_fix#index'
+  get '/heise_newsfeed(.format)' => 'hs_fix#index'
+  get '/de_morgen(.format)' => 'de_morgen_#index'
 end
